c22da55c3ee4cbe09e6716d879153582e7654231
Sene-Yazo <110239933+Sene-Yazo@users.noreply.github.com>
2022-08-09 20:54:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,23 +11,22 @@ from igramscraper.instagram import Instagram
 from dotenv import load_dotenv
 load_dotenv()
 
-DEBUG = False
-
+DEBUG = True
 # Telegram variables
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("5509535105:AAHRRi4KpRlPVDXw4xlvKkEMRjRpdBD9ZPo")
 
 # Instagram variables
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
+USERNAME = os.getenv("gym_kzmetk_akssuar")
+PASSWORD = os.getenv("enes.2112")
 
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_URL = os.getenv("8b85c6e8-d393-40de-a713-c14cf4b3ce04")
 
 bot = telebot.TeleBot(TOKEN, threaded=True)
 
-client = Client(USERNAME, PASSWORD)
+client = Client("gym_kzmetk_akssuar", "enes.2112")
 
-ADMIN_ID = os.getenv("ADMIN_ID")
-GROUP_ID = os.getenv("GROUP_ID")
+ADMIN_ID = os.getenv("2122568291")
+GROUP_ID = os.getenv("-686372494")
 
 insta = Instagram()
 
